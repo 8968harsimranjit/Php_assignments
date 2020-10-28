420c56619dc78bb00ff8e8e377d3c2026482ab9e
8968harsimranjit <61202199+8968harsimranjit@users.noreply.github.com>
2020-10-27 22:02:41 -0700

    Update week_07.php
    Editing Week_07 assignment

--- week_07.php
+++ week_07.php
@@ -1,13 +1,6 @@
 <?php
 $submitted_email = isset($_POST['email']);
 $submitted_password = isset($_POST['password']);
-// if ($email != 'homer@aol.com') {
-//     header("Location: http://localhost/dgl123_assignment_5/login.php");
-//     die;
-// } elseif ($password != 'flanders') {
-//     header("Location: http://localhost/dgl123_assignment_5/login.php");
-//     die;
-// }
 if ($submitted_email == 'marge' && $submitted_password == 'maggie77') {
 echo 'Welcome';
 } else {
