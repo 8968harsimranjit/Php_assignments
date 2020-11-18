e6cd5595e0464148964762fd39b8d709bd8e8669
8968harsimranjit <61202199+8968harsimranjit@users.noreply.github.com>
2020-11-18 00:43:12 -0800

    Add files via upload
    adding templates

---
+++ website/newsletter.php
@@ -0,0 +1,6 @@
+<form method = 'POST'>
+    <label for="email"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" required><br>
+     
+    <input type="submit" value="Sign UP" class='button'/>
+</form>
